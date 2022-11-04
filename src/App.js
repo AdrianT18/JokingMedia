1b2c944d613e099893b946e61a8b1fa65fe2a90f
AdrianT18 <100729061+AdrianT18@users.noreply.github.com>
2022-11-04 21:06:33 +0000

    Added Results section and @media for mobile phones

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import strat from './images/strat.png'
 import stratergy from './images/stratergy.png'
 
 
+
 function App() {
     return (
         <div className="container">
@@ -104,6 +105,78 @@ function App() {
                             d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
                             className="shape-fill"></path>
                     </svg>
+                </div>
+            </section>
+
+            <section className="strategy">
+                <header className="strat">
+                    <h1 className="strat-title"> Ready To <span className="scale"> Scale</span> Your Business? </h1>
+                    <h2 className="strat-sub">Here's <span className="how"> How </span> It Works </h2>
+                </header>
+
+                <div className="strat-images">
+                    <div className="first">
+                        <div className="circle">1</div>
+                        <img className="call-img" src={call} alt="mobile phone icon"/>
+                        <h2>Initial Call</h2>
+                    </div>
+
+                    <div className="second">
+                        <div className="circle">2</div>
+                        <img className="strat-img" src={stratergy} alt="thinking lightbulb"/>
+                        <h2>Strategy</h2>
+                    </div>
+
+                    <div className="third">
+                        <div className="circle">3</div>
+                        <img className="kick-off" src={sample} alt="paint sample"/>
+                        <h2> Kick Off</h2>
+                    </div>
+                </div>
+
+                <div className="step-1">
+                    <div className="strat1">
+                        <h3 className="heading1">Step 01</h3>
+                        <h2 className="subheading1">Initial Call</h2>
+                        <p className="step1-para">Although we wish we could help everyone, unfortunately we can’t. To
+                            ensure that we are a good fit and we know for sure we can help you, we get on an initial
+                            call just to conduct a growth analysis and understand how we will execute the strategy.</p>
+                    </div>
+                    <div className="strat1-img">
+                        <img className="calling-img" src={calling} alt="people connected around a mobile phone"/>
+                        <a className="scrollDown-4" href="#calandly">
+                        <p className="button-4">Speak To Our Team Today <br/>
+                            <span className="bigger-4">Schedule Your FREE Audit Call Now</span></p>
+                    </a>
+                    </div>
+                </div>
+
+                <div className="step-2">
+                    <div className="strat2-img">
+                        <img className="strategy-img2" src={strat} alt="vector of talking through phone"/>
+                    </div>
+                    <div className="strat2">
+                        <h3 className="heading2">Step 02</h3>
+                        <h2 className="subheading2"> Social Media Strategy</h2>
+                        <p className="step2-para">After the call I get to work straightaway and create a social media
+                            strategy alongside samples of content so you know what things will look like before even
+                            officially proceeding. This is done so you can review everything before deciding you want to
+                            work with us. (Which you would want to after seeing the strategy + samples)</p>
+                    </div>
+                </div>
+
+                <div className="step-3">
+                    <div className="strat3">
+                        <h3 className="heading3">Step 03</h3>
+                        <h2 className="subheading3"> Kick Off</h2>
+                        <p className="step3-para">Once everything is approved & the first official batch of content is
+                            ready, we are good to go! The next steps would be to start publishing the content alongside
+                            performing different actions and split testing different things in order to get our content
+                            in front of our target audience and convert them into followers.</p>
+                    </div>
+                    <div className="strat3-img">
+                        <img className="kickOff-img" src={sampleMeeting} alt="vector of a meeting"/>
+                    </div>
                 </div>
             </section>
 
