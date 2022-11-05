499bc22dd01365dc2d125cbcb84a12afe4b1176f
AdrianT18 <100729061+AdrianT18@users.noreply.github.com>
2022-11-05 21:08:21 +0000

    Added strategy section added/fixed @media for all sections so far

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './offer.css'
 import './results.css'
 import './strategy.css'
 import mobilePhone from './images/phone.png'
-import resultImage from './images/results.png'
+import resultImage from './images/results-image.png'
 import call from './images/call.png'
 import calling from './images/calling.png'
 import sample from './images/sample.png'
@@ -119,32 +119,31 @@ function App() {
             <section className="strategy">
                 <header className="strat">
                     <h1 className="strat-title"> Ready To <span className="scale"> Scale</span> Your Business? </h1>
-                    <h2 className="strat-sub">Here's <span className="how"> How </span> It Works </h2>
+                    <h2 className="strat-sub">Here's <span className="how"> How </span> It Works</h2>
                 </header>
 
                 <div className="strat-images">
                     <div className="first">
                         <div className="circle">1</div>
                         <img className="call-img" src={call} alt="mobile phone icon"/>
-                        <h2>Initial Call</h2>
+                        <h2 className="sub">Initial Call</h2>
                     </div>
 
                     <div className="second">
                         <div className="circle">2</div>
                         <img className="strat-img" src={stratergy} alt="thinking lightbulb"/>
-                        <h2>Strategy</h2>
+                        <h2 className="sub">Strategy</h2>
                     </div>
 
                     <div className="third">
                         <div className="circle">3</div>
                         <img className="kick-off" src={sample} alt="paint sample"/>
-                        <h2> Kick Off</h2>
+                        <h2 className="sub"> Kick Off</h2>
                     </div>
                 </div>
-
                 <div className="step-1">
                     <div className="strat1">
-                        <h3 className="heading1">Step 01</h3>
+                        <h3 className="heading1"><span className="word">Step</span> 01</h3>
                         <h2 className="subheading1">Initial Call</h2>
                         <p className="step1-para">Although we wish we could help everyone, unfortunately we can’t. To
                             ensure that we are a good fit and we know for sure we can help you, we get on an initial
@@ -158,13 +157,12 @@ function App() {
                     </a>
                     </div>
                 </div>
-
                 <div className="step-2">
                     <div className="strat2-img">
                         <img className="strategy-img2" src={strat} alt="vector of talking through phone"/>
                     </div>
                     <div className="strat2">
-                        <h3 className="heading2">Step 02</h3>
+                        <h3 className="heading2"><span className="word">Step</span> 02</h3>
                         <h2 className="subheading2"> Social Media Strategy</h2>
                         <p className="step2-para">After the call I get to work straightaway and create a social media
                             strategy alongside samples of content so you know what things will look like before even
@@ -175,7 +173,7 @@ function App() {
 
                 <div className="step-3">
                     <div className="strat3">
-                        <h3 className="heading3">Step 03</h3>
+                        <h3 className="heading3"><span className="word">Step</span> 03</h3>
                         <h2 className="subheading3"> Kick Off</h2>
                         <p className="step3-para">Once everything is approved & the first official batch of content is
                             ready, we are good to go! The next steps would be to start publishing the content alongside
@@ -185,6 +183,14 @@ function App() {
                     <div className="strat3-img">
                         <img className="kickOff-img" src={sampleMeeting} alt="vector of a meeting"/>
                     </div>
+                </div>
+                <div className="strat-wave">
+                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"
+                         preserveAspectRatio="none">
+                        <path
+                            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                            className="shape-fill"></path>
+                    </svg>
                 </div>
             </section>
 
